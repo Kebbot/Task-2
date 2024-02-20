e0a73c1635141da5aa4ee430688652de56e6c033
Nikolz <126600682+Kebbot@users.noreply.github.com>
2024-02-20 22:25:02 +0300

    Task-2

---
+++ Task/Task.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int dol, cent;
+	double number;
+	cout << "Введите сумму (например: 12.0): ";
+	cin >> number;
+	dol = number;
+	cent = (number - dol) * 100;
+	cout << "Указанная сумма: " << dol << " долларов" << "\t" << cent << " центов.\n";
+
+
+
+	return 0;
+}
